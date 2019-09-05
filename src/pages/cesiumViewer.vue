9b578a3748606662cc91f104a4bb2251a4659ef1
hemeng <993031732@qq.com>
2019-09-05 18:03:12 +0800

    CesiumViewer token

--- src/pages/cesiumViewer.vue
+++ src/pages/cesiumViewer.vue
@@ -28,8 +28,40 @@
       }
     },
     mounted () {
-      //创建viewer实例
-      this.viewer = new this.Cesium.Viewer('cesiumContainer', this.config);
+      let Cesium = this.Cesium;
+
+      // 资源访问令牌 Cesium token
+      Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2OTgxNTY1Ni05ZWQzLTQ5OWMtODVlMS1kZTQyNzE5ODkyMzYiLCJpZCI6MTUzNDgsInNjb3BlcyI6WyJhc2wiLCJhc3IiLCJhc3ciLCJnYyJdLCJpYXQiOjE1Njc2NTMzMzZ9.lgaMvwkiWOimFhfJ4dgfNmKZDfjqIPaAfg0ycjwFTfc';
+      
+      // 创建viewer实例
+      let viewer = new Cesium.Viewer('cesiumContainer', this.config);
+      //增加地图图片资源提供者（CesiumIon） Cesium官方
+      viewer.imageryLayers.remove(0); // 图片层级管理 移除默认的基础层级提供者
+      viewer.imageryLayers.addImageryProvider(new Cesium.IonImageryProvider({assetId:3954}));
+
+      //设置地形
+      viewer.terrainProvider=Cesium.createWorldTerrain({
+        requestWaterMask:true,//水渲染需求
+        requestVertexNormals:true//顶点法线渲染需求
+      });
+
+      viewer.scene.globe.depthTestAgainstTerrain=true; //深度显示（用于湖泊河流 水深对周围景观的影响）
+      viewer.scene.globe.enableLighting=true; //全局日照（受太阳，月亮的位置而影响光照信息）
+  
+      //创建初始化摄像机视图
+      var initialPosition=new Cesium.Cartesian3.fromDegrees(110.998114468289017509, 30.674512895646692812, 2631000); //摄像机位置 ，经度，纬度，高度
+      var initialOrientation=new Cesium.HeadingPitchRoll.fromDegrees(7.1077496389876024807, -91.987223091598949054, 0.025883251314954971306);//飞行 专用的  表示旋转角度之类的东西： 飞行中飞机机体轴相对于地面的角位置
+      var homeCameraView={
+        destination:initialPosition,
+        orientation:{
+          heading:initialOrientation.heading, //偏航角
+          pitch:initialOrientation.pitch,     //俯仰角
+          roll:initialOrientation.roll        //滚转角
+        }
+      };
+      viewer.scene.camera.setView(homeCameraView);
+
+      this.viewer = viewer;
     }
   }
 </script>
